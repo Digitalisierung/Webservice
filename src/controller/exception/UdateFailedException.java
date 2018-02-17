d6ca514f99d358c5b1feb9445211c8dd84b134b6
ukar2 <juri.rempel@software-development.net>
2018-02-17 19:46:15 +0100

    Neue Exception Klasse erstellt

---
+++ src/controller/exception/UdateFailedException.java
@@ -0,0 +1,18 @@
+package controller.exception;
+
+public class UdateFailedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UdateFailedException() {
+		super();
+	}
+	
+	public UdateFailedException(String message) {
+		super(message);
+	}
+
+}
